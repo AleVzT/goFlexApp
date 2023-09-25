f75161fb966cd20c847e9b520ac93f30cb6bbd52
Alexandro Garcia <agarcialg66@gmail.com>
2023-09-25 17:30:34 -0300

    update data

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,5 +1,4 @@
 
-
 export const environments = {
   production: true,
   baseUrl: 'http://104.197.164.124:4000/api'
--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,5 +1,6 @@
 
 export const environments = {
   production: false,
-  baseUrl: 'http://localhost:4000/api'
+  // baseUrl: 'http://localhost:4000/api'
+  baseUrl: 'http://104.197.164.124:4000/api'
 }
