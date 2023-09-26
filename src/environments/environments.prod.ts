9d506c1b65c08f232200c61f81c4644f8c0d74a5
Alexandro Garcia <agarcialg66@gmail.com>
2023-09-25 21:31:37 -0300

    se cambia el puerto del servidor

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,5 +1,5 @@
 
 export const environments = {
   production: true,
-  baseUrl: 'http://104.197.164.124:4000/api'
+  baseUrl: 'http://104.197.164.124:443/api'
 }
--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,6 +1,5 @@
 
 export const environments = {
   production: false,
-  // baseUrl: 'http://localhost:4000/api'
-  baseUrl: 'http://104.197.164.124:4000/api'
+  baseUrl: 'http://localhost:4000/api'
 }
